dc7e08c62ba2380bfca5443cb7b968cfe404206e
BushyScripter <typlezbrand@gmail.com>
2025-12-29 00:54:09 -0500

    www

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ pool.query(`
         is_premium INTEGER DEFAULT 0
     )
 `).catch(err => console.error("DB Setup Error:", err));
-pool.query(`UPDATE users SET is_premium = 1 WHERE email = 'TYPLEZBRAND@GMAIL.COM'`);
+
 
 // --- Helper: Sync Subscription Status ---
 async function checkStripeStatus(user) {
